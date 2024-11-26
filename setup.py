8d4b80b59b6f30aa0acd516acf10ad559fee91c7
Matteo Pinna <matteo.pinna@hotmail.com>
2024-11-27 00:38:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'randresnet',
-    version = '0.1',
-    author = 'Matteo Pinna',
-    author_email = 'nennomp@gmail.com',
-    description = 'Randomized Convolutional Residual Neural Networks paper code',
-    url = 'https://github.com/NennoMP/randresnet',
+    name='randresnet',
+    version='0.1',
+    author='Matteo Pinna',
+    author_email='nennomp@gmail.com',
+    description='Randomized Convolutional Residual Neural Networks paper code',
+    url='https://github.com/NennoMP/randresnet',
     packages=find_packages(),
     python_requires='>=3.12',
 )
